e5ab3a0eba4b00f4a61971e909fd6e93c3785666
Caio Alexandre <contato.kaiyo@gmail.com>
2021-01-28 23:29:24 -0300

    Add styled-components module to project

--- components/grids.js
+++ components/grids.js
@@ -1,11 +1,23 @@
-import styles from '../styles/Grids.module.css'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  border: #eee solid 2px;
+  border-radius: 4px;
+  padding: 15px 35px;
+  transition: 0.5s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+
+  &:hover {
+    transform: scale(1.05);
+    border: #ccc solid 2px;
+  }
+`
 
 function ContentGrid(props) {
   const children = props.children
   return (
-    <div className={styles.content_grid}>
+    <Container>
       <span>{children}</span>
-    </div>
+    </Container>
   )
 }
 
